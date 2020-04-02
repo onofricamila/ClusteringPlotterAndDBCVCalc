330a60994efab3eae2838b9e69ae12846bb84035
Camila Onofri <onofricamila2@gmail.com>
2020-04-02 12:56:38 -0300

    typo

--- main.py
+++ main.py
@@ -7,10 +7,11 @@ from utils.clustering_managers.non_time_series_clustering_mngr import NonTimeser
 # by running this file, you will get
 
 # time series ----------------------------------
-# print("\n" + "DenStream")
+# dataContext
 dataContext = [BoundingBox(minimun=-2 , maximun=2),
                BoundingBox(minimun=-2 , maximun=2)]
 
+# print("\n" + "DenStream")
 denStreamClusMngr = DenstreamClusteringManager(dataContext)
 denStreamClusMngr.main()
 
@@ -23,9 +24,11 @@ dyCleeClusMngr = DycleeClusteringManager(dataContext)
 dyCleeClusMngr.main()
 
 
-#non time series ----------------------------------
+# non time series ----------------------------------
+# dataContext
 dataContext = [BoundingBox(minimun=-3 , maximun=3),
                BoundingBox(minimun=-3 , maximun=3)]
+
 print("\n" + "Non time series clustering")
 nonTimeSeriesClusMngr = NonTimeseriesClusteringMngr(dataContext)
 nonTimeSeriesClusMngr.main()
